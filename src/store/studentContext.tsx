6b19aa44c6ceeec21650bfd74d3438696cadc0ee
Will D <diweizhi@gmail.com>
2025-12-15 19:04:01 +1100

    reset

--- src/store/studentContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import type { StudentContextValue } from '../types/student';
-
-export const StudentContext = createContext<StudentContextValue>({
-  fetchStudents: () => {},
-});
