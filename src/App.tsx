44d610a48ba39273aedf7bddeb9def9ddf80468f
Will D <diweizhi@gmail.com>
2025-12-16 15:56:07 +1100

    useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useFetch } from './hooks/useFetch';
+import { HTTP_METHODS, Student } from './types/student';
 
 const App = () => {
+  const {
+    data: students,
+    isLoading,
+    error,
+    fetchData: fetchStudents,
+  } = useFetch<Student[], null>({ method: HTTP_METHODS.GET, url: 'students' });
+
+  console.log(students);
   return <div></div>;
 };
 
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,1 +1,45 @@
-export const useFetch = () => {};
+import { useState } from 'react';
+import { FetchRequestConfig, HTTP_METHODS } from '../types/student';
+
+export const useFetch = <T = any, P = void>(
+  config: FetchRequestConfig,
+  onSuccess?: () => void
+) => {
+  const [data, setData] = useState<T>(
+    (Array.isArray([] as T) ? [] : null) as T
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async (payload?: P) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const baseUrl = '/api';
+      const options: RequestInit = {
+        method: config.method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      if (
+        options.method === HTTP_METHODS.POST ||
+        options.method === HTTP_METHODS.PUT
+      ) {
+        options.body = JSON.stringify(payload);
+      }
+      const response = await fetch(`${baseUrl}/${config.url}`, options);
+      const json = await response.json();
+      if (!response.ok) {
+        throw new Error((json as { error?: string }).error ?? 'Request Failed');
+      }
+      setData(json);
+      onSuccess?.();
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'unknown error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { data, isLoading, error, fetchData };
+};
